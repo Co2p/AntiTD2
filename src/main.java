7f9e7030de772285d6434e1057c9c5ff153a170a
Daniel Sjöström <d.sjostrom@me.com>
2016-12-07 11:01:11 +0100

    Main is now working properly. Added a new constructor to LeveLBuilder

--- src/main.java
+++ src/main.java
@@ -9,7 +9,14 @@ public class main {
     public static void main(String[] args ){
 
         ArrayList<Level> levelArray = new ArrayList<Level>();
-        LevelBuilder lb = new LevelBuilder(args[0]);
+        LevelBuilder lb;
+
+        if(args.length > 0) {
+            lb = new LevelBuilder(args[0]);
+        } else {
+            lb = new LevelBuilder();
+        }
+
         for (int i = 0; i < lb.getNoOfLevels(); i++) {
             Level l = lb.buildLevel(i);
             levelArray.add(l);
