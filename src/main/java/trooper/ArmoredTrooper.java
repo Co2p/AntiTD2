e9607f9d0e58a293881f827681bb4cf869be63dc
Alexander Nyström <dv15anm@cs.umu.se>
2016-12-13 16:12:55 +0100

    repaired armored trooper

--- src/main/java/trooper/ArmoredTrooper.java
+++ src/main/java/trooper/ArmoredTrooper.java
@@ -5,8 +5,42 @@ package trooper;
  */
 public class ArmoredTrooper extends Trooper {
 
-    public ArmoredTrooper(int hp) {
+    private int armor;
+    private int zombieArmor;
+
+    /**
+     *
+     * @param hp
+     * @param speed
+     */
+    public ArmoredTrooper(int hp, int speed) {
         super(hp);
-        setArmor(hp/2);
+        armor = 15;
+    }
+
+    public ArmoredTrooper(int hp, int speed, int armor) {
+        super(hp);
+        this.armor = armor;
+        zombieArmor = (armor/2);
+    }
+
+    public void receiveDamage(int removeHealth){
+        if(hasTurned()) {
+            armor =  zombieArmor;
+        }
+        removeHealth = removeHealth - (removeHealth * armor/100);
+
+        super.receiveDamage(removeHealth);
+        //TODO REMOVE SOUT
+        System.out.println("Target hp: " + getHealth());
+    }
+
+
+    /**
+     * Sets the armour strength
+     * @param armor armour strength
+     */
+    public void setArmor(int armor) {
+        this.armor = armor;
     }
 }
