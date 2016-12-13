12fc76adc43b0a6f413173b6941941c11247efed
Alexander Nyström <dv15anm@cs.umu.se>
2016-12-13 14:36:07 +0100

    partly restored game

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -82,6 +82,8 @@ public class Game extends JPanel implements Runnable {
         this.defense = new Defense(map,level.towerSpawnRate);
         shop = new Shop(army);
         gameContainer = new GameContainer();
+        gameContainer.setColumnCount(level.getColumns());
+        gameContainer.setRowCount(level.getRows());
         x = startPosition.getX();
         y = startPosition.getY();
     }
