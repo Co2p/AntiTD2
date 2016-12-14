7c2b14a3e1316792640abbb08f03c08ea3dc71ee
Co2p <go.co2p@gmail.com>
2016-12-14 15:10:12 +0100

    CamelCase

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -11,7 +11,6 @@ import java.util.Objects;
 import helpers.Translator;
 import helpers.Position;
 import tower.Defense;
-import tower.LaserTower;
 import trooper.*;
 import tile.*;
 
@@ -135,7 +134,7 @@ public class Game extends JPanel implements Runnable {
             }
         }
         shop.stopTime();
-        results.setCreditsused(shop.getNoOfCredits());
+        results.setCreditsUsed(shop.getNoOfCredits());
         results.setLevelName(level.getLevelName());
         results.setTime(shop.getTime());
         player.setResult(results);
